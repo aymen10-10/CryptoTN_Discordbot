fb2f8f2b0363bf9cedc34dcf9d4481a31542c947
aymen10-10 <mrabtiaymen65@gmail.com>
2025-04-18 19:59:54 +0100

    Update views.py

--- views.py
+++ views.py
@@ -1,1 +1,8 @@
+from discord.ui import View, Button, Modal, TextInput
+import discord
 
+class P2PMainMenu(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.add_item(Button(label="Je veux vendre", style=discord.ButtonStyle.success, custom_id="vendre"))
+        self.add_item(Button(label="Je veux acheter", style=discord.ButtonStyle.danger, custom_id="acheter"))
