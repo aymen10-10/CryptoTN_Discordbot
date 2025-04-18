863c9e90a3a6c8923da6a4982ae109842bcef0c9
aymen10-10 <mrabtiaymen65@gmail.com>
2025-04-18 20:03:10 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
-
+def format_offer(user, montant, prix, methode, identifiant, role):
+    return (
+        f"{'Nouvelle offre de vente' if role == 'vendeur' else 'Nouvelle demande d’achat'}\n"
+        f"Utilisateur : {user.mention}\n"
+        f"Montant : {montant} USDT\n"
+        f"Prix : 1 USDT = {prix} DT\n"
+        f"Méthode : {methode}\n"
+        f"Identifiant : {identifiant}"
+    )
