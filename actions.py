dd904da8f9b2c2fdb97dd6f02778fdee1a7ed16c
aymen10-10 <mrabtiaymen65@gmail.com>
2025-04-19 12:41:54 +0100

    Update actions.py

--- actions.py
+++ actions.py
@@ -9,7 +9,6 @@ async def show_sellers(interaction: Interaction):
     if not methodes:
         await interaction.response.send_message("Aucun vendeur disponible.", ephemeral=True)
         return
-
     await interaction.response.send_message("Sélectionnez un vendeur :", ephemeral=True)
 
 async def show_buyers(interaction: Interaction):
@@ -18,5 +17,4 @@ async def show_buyers(interaction: Interaction):
     if not methodes:
         await interaction.response.send_message("Aucun acheteur disponible.", ephemeral=True)
         return
-
     await interaction.response.send_message("Voici la liste des acheteurs disponibles :", ephemeral=True)
