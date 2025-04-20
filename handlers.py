937d16dbf49e82600ba7f2ce3f26a3122b6f028a
aymen10-10 <mrabtiaymen65@gmail.com>
2025-04-20 01:27:06 +0100

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,9 +1,21 @@
 from discord import Interaction
-import json
+from views import StartTransactionView
 from utils import load_database
 
 async def envoyer_offre_vendeur(interaction: Interaction, offre: dict):
-    from views import StartTransactionView  # import local pour éviter boucle
     buyer_id = interaction.user.id
-    view = StartTransactionView(buyer_id=buyer_id, offre=offre)
-    await interaction.response.send_message("Offre trouvée !", view=view, ephemeral=True)
+    view = StartTransactionView(offer=offre)
+    await interaction.response.send_message(
+        "Offre trouvée ! Cliquez pour démarrer la transaction :",
+        view=view,
+        ephemeral=True
+    )
+
+async def envoyer_offre_acheteur(interaction: Interaction, offre: dict):
+    seller_id = interaction.user.id
+    view = StartTransactionView(offer=offre)
+    await interaction.response.send_message(
+        "Offre trouvée ! Cliquez pour démarrer la transaction :",
+        view=view,
+        ephemeral=True
+    )
