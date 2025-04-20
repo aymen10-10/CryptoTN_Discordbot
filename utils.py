a5f1aa78ea74f73f2a26ffa2b6ba7786366e6bbf
aymen10-10 <mrabtiaymen65@gmail.com>
2025-04-20 01:55:50 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,11 +5,10 @@ DATABASE_FILE = "database.json"
 
 def load_database():
     if not os.path.exists(DATABASE_FILE):
-        return {"vendeurs": [], "acheteurs": [], "transactions": []}
-    
-    with open(DATABASE_FILE, "r", encoding="utf-8") as f:
-        return json.load(f)
+        return {"vendeurs": [], "acheteurs": []}
+    with open(DATABASE_FILE, "r", encoding="utf-8") as file:
+        return json.load(file)
 
 def save_database(data):
-    with open(DATABASE_FILE, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+    with open(DATABASE_FILE, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
