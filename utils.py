d7156ef0c61fd38fc8373d9b054c117136005699
aymen10-10 <mrabtiaymen65@gmail.com>
2025-04-20 01:31:38 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,18 @@
 import json
+import os
+
+DB_FILE = "database.json"
 
 def load_database():
-    try:
-        with open("database.json", "r") as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {"sell_offers": [], "buy_offers": []}
+    if not os.path.exists(DB_FILE):
+        with open(DB_FILE, "w") as f:
+            json.dump({"vendeurs": [], "acheteurs": []}, f)
 
-def save_database(data):
-    with open("database.json", "w") as f:
+    with open(DB_FILE, "r") as f:
+        return json.load(f)
+
+def save_offer(offer_type, offer_data):
+    data = load_database()
+    data[offer_type].append(offer_data)
+    with open(DB_FILE, "w") as f:
         json.dump(data, f, indent=4)
